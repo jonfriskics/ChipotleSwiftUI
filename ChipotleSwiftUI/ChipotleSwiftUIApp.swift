0e1657091da117e85286cde46720a8cd66e2d1bb
Jon Friskics <jon-friskics@pluralsight.com>
2020-07-27 21:17:53 -0400

    Initial Commit

---
+++ ChipotleSwiftUI/ChipotleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChipotleSwiftUIApp.swift
+//  ChipotleSwiftUI
+//
+//  Created by Jon Friskics on 7/27/20.
+//
+
+import SwiftUI
+
+@main
+struct ChipotleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
